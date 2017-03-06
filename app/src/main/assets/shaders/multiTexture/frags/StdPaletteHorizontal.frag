d0bb3467eb6415648bc7720bfb8ed79dbab3f983
HenryCo <hd2files@gmail.com>
2017-03-06 23:02:19 +0100

    API min 19 added geometry

--- app/src/main/assets/shaders/multiTexture/frags/StdPaletteHorizontal.frag
+++ app/src/main/assets/shaders/multiTexture/frags/StdPaletteHorizontal.frag
@@ -17,21 +17,35 @@ uniform vec3 u_line[2]; // Ax + By + C = 0
 
 void main() {
 
-    vec3 p_col = vec3(0.0);
     float trueHeight = 0.0;
+    vec3 p_col = vec3(0.0);
+    vec3 coeff[2];
+    coeff[0] = u_line[0];
+    coeff[1] = u_line[1];
 
     for (float y = 0.0; y < u_dimension.y; y += 1.0) {
-        vec2 point = vec2(v_TexCoordinate[1].s, y / u_dimension.y);
-        vec4 p_c = texture2D(u_Texture0, point).rgba;
 
-        if (p_c.a != 0.0) {
-            p_col += p_c.rgb;
-            trueHeight += 1.0;
+        vec2 pointNormed = vec2(v_TexCoordinate[1].s, y / u_dimension.y);
+        vec2 point = vec2(pointNormed.x * u_dimension.x, u_dimension.y * (1. - pointNormed.y));
+        vec4 pointColor = texture2D(u_Texture0, pointNormed).rgba;
+
+        if (pointColor.a != 0.0) {
+
+            for (int i = 0; i < 2; i++) {
+                if (coeff[i].x == 0.) coeff[i].x += 0.1;
+                if (coeff[i].y == 0.) coeff[i].y += 0.1;
+            }
+
+            float py1 = (-1.) * ((coeff[0].x * point.x) + coeff[0].z) / coeff[0].y;
+            float py2 = (-1.) * ((coeff[1].x * point.x) + coeff[1].z) / coeff[1].y;
+
+            if ((point.y > py1 && point.y < py2) || (point.y > py2 && point.y < py1)) {
+                p_col += pointColor.rgb;
+                trueHeight += 1.0;
+            }
         }
     }
 
     vec3 color = p_col / max(trueHeight, 1.0);
     gl_FragColor = vec4(color, 1.0);
-
-
 }
--- app/src/main/java/net/henryco/opalette/application/main/program/PaletteProgramHorizontal.java
+++ app/src/main/java/net/henryco/opalette/application/main/program/PaletteProgramHorizontal.java
@@ -32,6 +32,7 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 	public static final String VERT_FILE = OPallMultiTexture.DEF_SHADER+".vert";
 	public static final String FRAG_FILE = OPallMultiTexture.FRAG_DIR+"/StdPaletteHorizontal.frag";
 	public static final String u_dimension = "u_dimension";
+	public static final String u_line = "u_line";
 
 	private final long id;
 
@@ -46,7 +47,8 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 	private MultiTexture multiTexture;
 	private boolean uCan = false;
 	private OPallBar backBar;
-	private final float[] bitmap_size = {0,0};
+
+	private float[] lineCoeffs;
 
 	private OPallObservator observator;
 
@@ -73,6 +75,8 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 		multiTexture = new MultiTexture(context, VERT_FILE, FRAG_FILE, 2);
 		backBar = new BarHorizontal(context);
 		chessBox = new ChessBox(context);
+
+		lineCoeffs = new float[6];
 	}
 
 
@@ -87,17 +91,6 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 		multiTexture.setScreenDim(width, height);
 		backBar.createBar(width, height);
 		chessBox.setScreenDim(width, height);
-
-		float[] p1 = new float[]{100,0};
-		float[] p2 = new float[]{200, 100};
-
-
-		float a = OPallGeometry.lineAx(p1, p2);
-		float b = OPallGeometry.lineBy(p1, p2);
-		float c = OPallGeometry.lineC(p1, p2);
-
-		System.out.println("COEFF: "+a+" | "+b+" | "+c);
-
 	}
 
 
@@ -131,6 +124,7 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 				GLESUtils.clear(GLESUtils.Color.TRANSPARENT);
 				multiTexture.render(camera2D, program -> {
 					GLES20.glUniform2f(GLES20.glGetUniformLocation(program, u_dimension), width, height);
+					GLES20.glUniform3fv(GLES20.glGetUniformLocation(program, u_line), 2, lineCoeffs, 0);
 				});
 			});
 
@@ -177,9 +171,24 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 			multiTexture.setTexture(0, imageTexture);
 			multiTexture.setTexture(1, barSrcBuffer.getTexture());
 			multiTexture.setFocusOn(1);
-			bitmap_size[0] = bmpWidth * scale;
-			bitmap_size[1] = bmpHeight * scale;
 			uCan = true;
+
+			float[] point11 = {0,0};
+			float[] point12 = {bmpWidth * scale, 0};
+
+			float[] point21 = {0, bmpHeight * scale};
+			float[] point22 = {bmpWidth * scale, bmpHeight * scale};
+
+			float a1 = OPallGeometry.lineAx(point11, point12);
+			float b1 = OPallGeometry.lineBy(point11, point12);
+			float c1 = OPallGeometry.lineC(point11, point12);
+
+			float a2 = OPallGeometry.lineAx(point21, point22);
+			float b2 = OPallGeometry.lineBy(point21, point22);
+			float c2 = OPallGeometry.lineC(point21, point22);
+
+			lineCoeffs = new float[]{a1, b1, c1, a2, b2, c2};
+
 
 		});
 	}
@@ -188,5 +197,60 @@ public class PaletteProgramHorizontal implements OPallUnderProgram<ProtoActivity
 
 
 
+
+
+
+	public static final String FRAG_PROGRAM =
+			"#version 100\n" +
+			"precision mediump float;\n" +
+			"\n" +
+			"varying vec4 v_Position;\n" +
+			"varying vec4 v_WorldPos;\n" +
+			"varying vec2 v_TexCoordinate[5];\n" +
+			"\n" +
+			"uniform sampler2D u_Texture0;\n" +
+			"uniform sampler2D u_Texture1;\n" +
+			"uniform sampler2D u_Texture2;\n" +
+			"uniform sampler2D u_Texture3;\n" +
+			"uniform sampler2D u_Texture4;\n" +
+			"\n" +
+			"uniform vec2 u_dimension;\n" +
+			"uniform int u_texNumb;\n" +
+			"uniform vec3 u_line[2]; // Ax + By + C = 0\n" +
+			"\n" +
+			"void main() {\n" +
+			"\n" +
+			"    float trueHeight = 0.0;\n" +
+			"    vec3 p_col = vec3(0.0);\n" +
+			"    vec3 coeff[2];\n" +
+			"    coeff[0] = u_line[0];\n" +
+			"    coeff[1] = u_line[1];\n" +
+			"\n" +
+			"    for (float y = 0.0; y < u_dimension.y; y += 1.0) {\n" +
+			"\n" +
+			"        vec2 pointNormed = vec2(v_TexCoordinate[1].s, y / u_dimension.y);\n" +
+			"        vec2 point = vec2(pointNormed.x * u_dimension.x, u_dimension.y * (1. - pointNormed.y));\n" +
+			"        vec4 pointColor = texture2D(u_Texture0, pointNormed).rgba;\n" +
+			"\n" +
+			"        if (pointColor.a != 0.0) {\n" +
+			"\n" +
+			"            for (int i = 0; i < 2; i++) {\n" +
+			"                if (coeff[i].x == 0.) coeff[i].x += 0.1;\n" +
+			"                if (coeff[i].y == 0.) coeff[i].y += 0.1;\n" +
+			"            }\n" +
+			"\n" +
+			"            float py1 = (-1.) * ((coeff[0].x * point.x) + coeff[0].z) / coeff[0].y;\n" +
+			"            float py2 = (-1.) * ((coeff[1].x * point.x) + coeff[1].z) / coeff[1].y;\n" +
+			"\n" +
+			"            if ((point.y > py1 && point.y < py2) || (point.y > py2 && point.y < py1)) {\n" +
+			"                p_col += pointColor.rgb;\n" +
+			"                trueHeight += 1.0;\n" +
+			"            }\n" +
+			"        }\n" +
+			"    }\n" +
+			"\n" +
+			"    vec3 color = p_col / max(trueHeight, 1.0);\n" +
+			"    gl_FragColor = vec4(color, 1.0);\n" +
+			"}";
 
 }
