8697eee1fad5fe920e96c70cb32c9fc20f55eb60
HenryCo <hd2files@gmail.com>
2017-03-20 19:16:28 +0100

    Convolve filters in progress More programs Programmatically seek-bar updated and fixed Program pipe-line reworked Added new programs

---
+++ app/src/main/assets/shaders/default/frags/ConvolveFilter.frag
@@ -0,0 +1,41 @@
+precision mediump float;
+
+// necessary part
+varying vec4 v_Position;
+varying vec4 v_WorldPos;
+varying vec2 v_TexCoordinate;
+
+uniform sampler2D u_Texture0;
+
+
+// custom part
+uniform float u_matrixSize;  // 3, 5
+uniform float u_matrixDim;  // 9, 25
+uniform float u_matrix3[9];
+uniform float u_matrix5[25];
+
+uniform vec2 u_screenDim;
+
+
+
+void main() {
+
+    vec2 pos = gl_FragCoord.xy;
+    vec2 cor = vec2((u_matrixSize - 1.) / 2.);
+    vec3 rgb = vec3(0.);
+
+    for (float i = 0.; i < u_matrixSize; i++) {
+        for (float k = 0.; k < u_matrixSize; k++) {
+            vec2 ipos = vec2(i - cor.x, k - cor.y);
+            vec3 irgb = texture2D(u_Texture0, pos + ipos).rgb;
+
+            int n = int(i * u_matrixSize + k);
+            if (u_matrixSize == 3.) irgb *= u_matrix3[n];
+            else irgb *= u_matrix5[n];
+
+            rgb += irgb;
+        }
+    }
+
+    gl_FragColor = vec4(rgb, 1.);
+}
--- app/src/main/java/net/henryco/opalette/api/glES/render/graphics/shaders/textures/extend/ConvolveTexture.java
+++ app/src/main/java/net/henryco/opalette/api/glES/render/graphics/shaders/textures/extend/ConvolveTexture.java
@@ -13,7 +13,147 @@ import net.henryco.opalette.api.glES.render.graphics.shaders.textures.OPallTextu
 public class ConvolveTexture extends OPallTextureExtended {
 
 
+
+
+
+
+
+	private float[] original_filter_matrix = {};
+	private float[] work_filter_matrix = {};
+	private float center_scale = 1;
+	private int matrix_size = 0;
+
+	private boolean enable = false;
+
+	public ConvolveTexture(Context context) {
+		this(context, Filter.LINEAR);
+	}
+	public ConvolveTexture(Context context, Filter filter) {
+		super(context, filter, OPallTexture.DEF_SHADER+".vert", FRAG_DIR);
+	}
+	public ConvolveTexture(Bitmap image, Context context) {
+		this(image, context, Filter.LINEAR);
+	}
+	public ConvolveTexture(Bitmap image, Context context, Filter filter) {
+		super(image, context, filter, OPallTexture.DEF_SHADER+".vert", FRAG_DIR);
+	}
+	public ConvolveTexture() {
+		super(OPallTexture.DEFAULT_VERT_FILE, FRAG_FILE);
+	}
+	public ConvolveTexture(Bitmap image) {
+		this(image, Filter.LINEAR);
+	}
+	public ConvolveTexture(Bitmap image, Filter filter) {
+		super(image, filter, OPallTexture.DEFAULT_VERT_FILE, FRAG_FILE);
+	}
+
+
+	@Override
+	protected void render(int program, Camera2D camera) {
+		if (isEnable()) {
+//			TODO
+		} else {
+//			TODO
+		}
+	}
+
+
+
+
+	public ConvolveTexture setFilterMatrix(float ... matrix) {
+
+		if (matrix.length == 0 || (matrix.length == 1 && matrix[0] == -1))
+			setEnable(false);
+		if (Math.sqrt(matrix.length) % 2 == 0)
+			throw new RuntimeException(getClass().getName()
+					+ ": Filter matrix dimension must be 3x3, 5x5, 7x7, 9x9 ...");
+		original_filter_matrix = matrix;
+		matrix_size = original_filter_matrix.length;
+		return setCenterEffect(center_scale);
+	}
+
+	public ConvolveTexture setCenterEffect(float s) {
+
+		center_scale = s;
+		float[] matrix = new float[matrix_size];
+		int dim = (int) Math.sqrt(matrix_size);
+		int center = (int) (0.5f * (dim - 1f));
+		System.arraycopy(original_filter_matrix, 0, matrix, 0, matrix_size);
+		matrix[(center * dim) + center] *= center_scale;
+		work_filter_matrix = normalize(matrix);
+		return this;
+	}
+
+	private static float[] normalize(float[] matrix) {
+
+		float sum = 0;
+		for (float v : matrix) sum += v;
+		float n = sum / matrix.length;
+		if (n == 0) return matrix;
+		for (int i = 0; i < matrix.length; i++)
+			matrix[i] /= n;
+		return matrix;
+	}
+
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+
+
+
+	private static final String FRAG_DIR = OPallTexture.FRAG_DIR+"/ConvolveFilter.frag";
+	private static final String FRAG_FILE =
+			"precision mediump float;\n" +
+			"\n" +
+			"// necessary part\n" +
+			"varying vec4 v_Position;\n" +
+			"varying vec4 v_WorldPos;\n" +
+			"varying vec2 v_TexCoordinate;\n" +
+			"\n" +
+			"uniform sampler2D u_Texture0;\n" +
+			"\n" +
+			"\n" +
+			"// custom part\n" +
+			"uniform float u_matrixSize;  // 3, 5\n" +
+			"uniform float u_matrixDim;  // 9, 25\n" +
+			"uniform float u_matrix3[9];\n" +
+			"uniform float u_matrix5[25];\n" +
+			"\n" +
+			"uniform vec2 u_screenDim;\n" +
+			"\n" +
+			"\n" +
+			"\n" +
+			"void main() {\n" +
+			"\n" +
+			"    vec2 pos = gl_FragCoord.xy;\n" +
+			"    vec2 cor = vec2((u_matrixSize - 1.) / 2.);\n" +
+			"    vec3 rgb = vec3(0.);\n" +
+			"\n" +
+			"    for (float i = 0.; i < u_matrixSize; i++) {\n" +
+			"        for (float k = 0.; k < u_matrixSize; k++) {\n" +
+			"            vec2 ipos = vec2(i - cor.x, k - cor.y);\n" +
+			"            vec3 irgb = texture2D(u_Texture0, pos + ipos).rgb;\n" +
+			"\n" +
+			"            int n = int(i * u_matrixSize + k);\n" +
+			"            if (u_matrixSize == 3.) irgb *= u_matrix3[n];\n" +
+			"            else irgb *= u_matrix5[n];\n" +
+			"\n" +
+			"            rgb += irgb;\n" +
+			"        }\n" +
+			"    }\n" +
+			"\n" +
+			"    gl_FragColor = vec4(rgb, 1.);\n" +
+			"}";
+
+
 	public static final class matrices {
+
 		public static float[] m_identity(){
 			return new float[] {
 					0, 0, 0,
@@ -113,7 +253,6 @@ public class ConvolveTexture extends OPallTextureExtended {
 					1, 0, 0, 0, 1
 			};
 		}
-
 		public static float[] m_horizontalMotionBlur() {
 			return new float[] {
 					0, 0, 0, 0, 0,
@@ -123,7 +262,6 @@ public class ConvolveTexture extends OPallTextureExtended {
 					0, 0, 0, 0, 0
 			};
 		}
-
 		public static float[] m_unsharp() {
 			return new float[] {
 					1, 4, 6, 4, 1,
@@ -134,99 +272,5 @@ public class ConvolveTexture extends OPallTextureExtended {
 			};
 		}
 
-
 	}
-
-
-
-
-	private float[] original_filter_matrix = {};
-	private float[] work_filter_matrix = {};
-	private float center_scale = 1;
-	private int matrix_size = 0;
-
-	private boolean enable = false;
-
-	public ConvolveTexture(Context context) {
-		this(context, Filter.LINEAR);
-	}
-	public ConvolveTexture(Context context, Filter filter) {
-		super(context, filter, OPallTexture.DEF_SHADER+".vert", FRAG_DIR);
-	}
-	public ConvolveTexture(Bitmap image, Context context) {
-		this(image, context, Filter.LINEAR);
-	}
-	public ConvolveTexture(Bitmap image, Context context, Filter filter) {
-		super(image, context, filter, OPallTexture.DEF_SHADER+".vert", FRAG_DIR);
-	}
-	public ConvolveTexture() {
-		super(OPallTexture.DEFAULT_VERT_FILE, FRAG_FILE);
-	}
-	public ConvolveTexture(Bitmap image) {
-		this(image, Filter.LINEAR);
-	}
-	public ConvolveTexture(Bitmap image, Filter filter) {
-		super(image, filter, OPallTexture.DEFAULT_VERT_FILE, FRAG_FILE);
-	}
-
-
-	@Override
-	protected void render(int program, Camera2D camera) {
-		if (isEnable()) {
-//			TODO
-		} else {
-//			TODO
-		}
-	}
-
-
-
-
-	public ConvolveTexture setFilterMatrix(float ... matrix) {
-
-		if (matrix.length == 0 || (matrix.length == 1 && matrix[0] == -1))
-			setEnable(false);
-		if (Math.sqrt(matrix.length) % 2 == 0)
-			throw new RuntimeException(getClass().getName()
-					+ ": Filter matrix dimension must be 3x3, 5x5, 7x7, 9x9 ...");
-		original_filter_matrix = matrix;
-		matrix_size = original_filter_matrix.length;
-		return setCenterEffect(center_scale);
-	}
-
-	public ConvolveTexture setCenterEffect(float s) {
-
-		center_scale = s;
-		float[] matrix = new float[matrix_size];
-		int dim = (int) Math.sqrt(matrix_size);
-		int center = (int) (0.5f * (dim - 1f));
-		System.arraycopy(original_filter_matrix, 0, matrix, 0, matrix_size);
-		matrix[(center * dim) + center] *= center_scale;
-		work_filter_matrix = normalize(matrix);
-		return this;
-	}
-
-	private static float[] normalize(float[] matrix) {
-
-		float sum = 0;
-		for (float v : matrix) sum += v;
-		float n = sum / matrix.length;
-		if (n == 0) return matrix;
-		for (int i = 0; i < matrix.length; i++)
-			matrix[i] /= n;
-		return matrix;
-	}
-
-
-	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}
-
-	private static final String FRAG_DIR = OPallTexture.FRAG_DIR+"/EditableDefault.frag";
-	private static final String FRAG_FILE = "";
-
 }
