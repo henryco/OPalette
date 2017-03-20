20a1dec2cf8b8302ed9cfeb460f5b859da2251e5
HenryCo <hd2files@gmail.com>
2017-03-20 17:53:22 +0100

    Convolve filters in progress More programs Programmatically seek-bar updated and fixed Program pipe-line reworked Added new programs

--- app/src/main/java/net/henryco/opalette/api/glES/render/graphics/shaders/textures/extend/ConvolveTexture.java
+++ app/src/main/java/net/henryco/opalette/api/glES/render/graphics/shaders/textures/extend/ConvolveTexture.java
@@ -13,6 +13,100 @@ import net.henryco.opalette.api.glES.render.graphics.shaders.textures.OPallTextu
 public class ConvolveTexture extends OPallTextureExtended {
 
 
+	public static final class matrices {
+		public static float[] m_identity(){
+			return new float[] {
+					0, 0, 0,
+					0, 1, 0,
+					0, 0, 0
+			};
+		}
+		public static float[] m_boxBlur() {
+			return new float[] {
+					1, 1, 1,
+					1, 1, 1,
+					1, 1, 1
+			};
+		}
+		public static float[] m_gaussianBlur() {
+			return new float[] {
+					1, 2, 1,
+					2, 4, 2,
+					1, 2, 1
+			};
+		}
+		public static float[] m_sharpen1() {
+			return new float[] {
+					0, -1, 0,
+					-1, 5, -1,
+					0, -1, 0
+			};
+		}
+		public static float[] m_sharpen2() {
+			return new float[] {
+					-1, -1, -1,
+					-1, 0, -1,
+					-1, -1, -1
+			};
+		}
+		public static float[] m_sharpen3() {
+			return new float[]{
+					-1, -1, -1,
+					-1, 8, -1,
+					-1, -1, -1
+			};
+		}
+		public static float[] m_sharpen4() {
+			return new float[]{
+					1, -2, 1,
+					-2, 5, -2,
+					1, -2, 1
+			};
+		}
+		public static float[] m_sharpen5() {
+			return new float[] {
+					-1, -1, -1, -1, -1,
+					-1, 3, 4, 3, -1,
+					-1, 4, 13, 4, -1,
+					-1, 3, 4, 3, -1,
+					-1, -1, -1, -1, -1
+			};
+		}
+
+		public static float[] m_diagShatter() {
+			return new float[] {
+					1, 0, 0, 0, 1,
+					0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0,
+					1, 0, 0, 0, 1
+			};
+		}
+
+		public static float[] m_horizontalMotionBlur() {
+			return new float[] {
+					0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0,
+					2, 3, 4, 5, 6,
+					0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0
+			};
+		}
+
+		public static float[] m_unsharp() {
+			return new float[] {
+					1, 4, 6, 4, 1,
+					4, 16, 24, 16, 4,
+					6, 24, -476, 24, 6,
+					4, 16, 24, 16, 4,
+					1, 4, 6, 4, 1
+			};
+		}
+	}
+
+
+
+
 	private float[] original_filter_matrix = {};
 	private float[] work_filter_matrix = {};
 	private float center_scale = 1;
